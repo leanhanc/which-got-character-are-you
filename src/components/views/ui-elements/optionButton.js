f26d1f7dc4918c2d2cb8a370fdd662562fbdb5f5
leo10099 <leandrohanc@gmail.com>
2018-06-05 10:47:58 -0300

    botonera css breakpoints

--- src/components/views/ui-elements/optionButton.js
+++ src/components/views/ui-elements/optionButton.js
@@ -1,6 +1,34 @@
 import styled from 'styled-components'
 
 const optionbutton = styled.button`
+
+@media only screen and (min-width: 320px) and (max-width: 600px) {
+  padding: 15px 20px;
+  border-radius: 7px;
+  border: 1.5px solid darkgray;
+  background-color: transparent;
+  font-weight: bolder;
+  font-family: 'Inconsolata', monospace;
+  letter-spacing: 2px;
+  color: lightgrey;
+  cursor: pointer;
+  margin: 32px auto;
+  display: block}
+
+  @media only screen and (min-width: 600px) and (max-width: 1024px) {
+    padding: 15px 20px;
+    border-radius: 7px;
+    border: 1.5px solid darkgray;
+    background-color: transparent;
+    font-weight: bolder;
+    font-family: 'Inconsolata', monospace;
+    letter-spacing: 2px;
+    color: lightgrey;
+    cursor: pointer;
+    margin: 32px auto;
+    display: block}
+
+@media only screen and (min-width: 1024px) {
 padding: 15px 20px;
 border-radius: 7px;
 border: 1.5px solid darkgray;
@@ -11,6 +39,6 @@ letter-spacing: 2px;
 color: lightgrey;
 cursor: pointer;
 margin: 8px 16px;
-display: inline-block;`
+display: inline-block;}`
 
 export default optionbutton
