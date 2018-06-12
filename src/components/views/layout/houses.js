14cd68fda9c58f5f26af7db535ca641fcccea40a
leo10099 <leandrohanc@gmail.com>
2018-06-12 18:17:26 -0300

    Agregado atributo ALT a todas las imagenes

--- src/components/views/layout/houses.js
+++ src/components/views/layout/houses.js
@@ -17,13 +17,14 @@ const Houses = () => (
       <img
         src='/img/house2.png'
         className='house has-text-centered p-b-32 m-r-64 scale6'
-        alt='Casa Stark'
+        alt='Casa Targaryen'
       />
       <img
         src='/img/house3.png'
-        className='house has-text-centered p-b-32 m-r-64 scale6'
+        className='house has-text-centered'
         alt='Casa Lannister'
       />
+      <img src='/img/4houses.jpg' className='mobileHouses' />
     </HouseImage>
 
   </div>
