8565a01cfddece9f1cf75e9c2c58d298bcb640af
leo10099 <leandrohanc@gmail.com>
2020-07-29 22:38:05 -0300

    Add react-modal. Use it in Postgame.

--- src/pages/Postgame/Postgame.jsx
+++ src/pages/Postgame/Postgame.jsx
@@ -1,11 +1,25 @@
 import React, { useState, useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import Modal from 'react-modal';
 
 // Helpers
 import { calculateCharacterPositions, buildTextResult } from './Postgame.helpers';
 
 // Styles
 import './Postgame.css';
+
+Modal.setAppElement('#root');
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
 
 function Postgame({ characterScore, lang }) {
   // Local State
@@ -15,6 +29,9 @@ function Postgame({ characterScore, lang }) {
   // Refs
   const scoreRef = useRef(characterScore);
   const langRef = useRef(lang);
+
+  // Helpers
+  const shouldShowModal = mainResult && resultExtraInfo;
 
   // Effects
   useEffect(() => {
@@ -27,9 +44,15 @@ function Postgame({ characterScore, lang }) {
 
   return (
     <div id="Postgame" className="postgame">
+      <Modal
+        isOpen={shouldShowModal}
+        overlayClassName="postgame-modal-overlay"
+        className="postgame-modal-content"
+      >
+        <h3>{mainResult}</h3>
+        <h5>{resultExtraInfo}</h5>
+      </Modal>
       <h1>POSTGAME PAGE</h1>
-      <h3>{mainResult}</h3>
-      <h5>{resultExtraInfo}</h5>
     </div>
   );
 }
