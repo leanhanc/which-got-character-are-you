1be044477d672f30c89a11f875f155ba3239e275
leo10099 <leandrohanc@gmail.com>
2020-08-02 14:05:59 -0300

    Fix getDifference and export

--- src/pages/Postgame/Postgame.helpers.js
+++ src/pages/Postgame/Postgame.helpers.js
@@ -1,22 +1,24 @@
 export const calculateCharacterPositions = (characterAnswers = {}) =>
   Object.keys(characterAnswers).sort((a, b) => characterAnswers[a] - characterAnswers[b]);
 
-const getDifference = (characterScore = []) => {
-  if (characterScore[5] - characterScore[4] === 0) {
-    return { ES: 'te parecés muchísimo a ', EN: 'You are very very similar to ' };
+export const getDifference = (characterScore = {}) => {
+  const ranking = Object.values(characterScore).sort((a, b) => a - b);
+
+  if (ranking[5] - ranking[4] === 0) {
+    return { ES: 'sos extremadamente similar a ', EN: 'you are extremely similar to ' };
   }
-  if (characterScore[5] - characterScore[4] === 1) {
+  if (ranking[5] - ranking[4] === 1) {
     return {
       ES: 'te parecés mucho a ',
       EN: 'you are very similar to ',
     };
   }
-  if (characterScore[5] - characterScore[4] === 1) {
+  if (ranking[5] - ranking[4] === 2) {
     return { ES: 'te parecés a ', EN: 'you are similar to ' };
   } else {
     return {
-      ES: 'tenés algunas cositas de',
-      EN: 'you are a bit similar to',
+      ES: 'tenés algunas cositas de ',
+      EN: 'you are a bit like ',
     };
   }
 };
